fa211897392947dfcf2f6ac6caaefd8ff6dc69b0
nguyenhuudung201 <115616484+nguyenhuudung201@users.noreply.github.com>
2023-02-03 13:48:05 +0700

    Update infomation_customer_add.sql

--- infomation_customer_add.sql
+++ infomation_customer_add.sql
@@ -9,7 +9,19 @@ select * from infomation_customer
 delete  from  infomation_customer
 dbcc checkident ( infomation_customer,reseed,0)
 insert into  register (phone_number,type_register,date_register,id_customer )
-values ('123456789','tra truoc','20021212',1)insert into  register (phone_number,type_register,date_register,id_customer )	values ('55555555','tra sau','20000212',1),			('777777','tra sau','20120506',2),			('55555655','tra truoc','20000212',3),			('777775','tra sau','20120506',4),			('55555455','tra sau','20000212',5),			('7777778','tra sau','20120506',2)select * from registerselect * from register where phone_number = 123456789select * from  infomation_customer where identify= 123456789select * from register where date_register='091212'SELECT *
+values ('123456789','tra truoc','20021212',1)
+insert into  register (phone_number,type_register,date_register,id_customer )
+	values ('55555555','tra sau','20000212',1),
+			('777777','tra sau','20120506',2),
+			('55555655','tra truoc','20000212',3),
+			('777775','tra sau','20120506',4),
+			('55555455','tra sau','20000212',5),
+			('7777778','tra sau','20120506',2)
+select * from register
+select * from register where phone_number = 123456789
+select * from  infomation_customer where identify= 123456789
+select * from register where date_register='091212'
+SELECT *
 FROM infomation_customer
 LEFT JOIN register
 ON infomation_customer.id = register.id_customer where identify=123456789;
